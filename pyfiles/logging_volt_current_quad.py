f3ef997626f0b3643dd67d338910e0e4e09cb1b9
hiroki-0001 <touhu.273@gmail.com>
2023-06-15 16:12:29 +0900

    Converted the data to be log to csv format

--- pyfiles/logging_volt_current_quad.py
+++ pyfiles/logging_volt_current_quad.py
@@ -7,6 +7,7 @@ import os
 import sys
 import subprocess
 import chardet
+import csv
 
 # INA226 I2C Slave address
 INA226_ADDR_A0_GND_A1_GND = 0x40
@@ -28,107 +29,91 @@ INA226_ADDR_A0_SCL_A1_SCL = 0x55
 
 def main():
     #INA226(i2c_Bus, i2c_slave_address, shunt_resistor_val)
-    Switched_mode_power = INA226_lib.INA226(8, INA226_ADDR_A0_GND_A1_GND, 2)
-    Battery_power = INA226_lib.INA226(8, INA226_ADDR_A0_VDD_A1_GND, 2)
-    SBC_power = INA226_lib.INA226(8, INA226_ADDR_A0_SDA_A1_GND, 2)
-    Motor_power = INA226_lib.INA226(8, INA226_ADDR_A0_SCL_A1_GND, 2)
-    Switched_mode_power.Initialization()
-    Battery_power.Initialization()
-    SBC_power.Initialization()
-    Motor_power.Initialization()
+    Switching_Power_Input = INA226_lib.INA226(8, INA226_ADDR_A0_GND_A1_GND, 2)
+    Battery_Input = INA226_lib.INA226(8, INA226_ADDR_A0_VDD_A1_GND, 2)
+    SBC_Power_Supply = INA226_lib.INA226(8, INA226_ADDR_A0_SDA_A1_GND, 2)
+    Actuator_Power_Supply = INA226_lib.INA226(8, INA226_ADDR_A0_SCL_A1_GND, 2)
     
-    tmp_file_fd = open("/var/tmp/voltage_and_current","w", encoding="utf_8", newline='\n')
-    tmp_file_fd.close()
-    os.chmod("/var/tmp/voltage_and_current", 0o777)
-    os.chown("/var/tmp/voltage_and_current", 1000, 1000)
+    Switching_Power_Input.Initialization()
+    Battery_Input.Initialization()
+    SBC_Power_Supply.Initialization()
+    Actuator_Power_Supply.Initialization()
     
-    if(os.path.isfile('/var/tmp/voltage_and_current_log')):
-        os.chmod("/var/tmp/voltage_and_current_log", 0o777)
-        os.chown("/var/tmp/voltage_and_current_log", 1000, 1000)
+    if(os.path.isfile('/var/tmp/voltage_and_current.csv')):
+        os.chmod("/var/tmp/voltage_and_current.csv", 0o777)
+        os.chown("/var/tmp/voltage_and_current.csv", 1000, 1000)
+    
+    else:
+        with open('/var/tmp/voltage_and_current.csv', 'w') as f:
+        os.chmod("/var/tmp/voltage_and_current.csv", 0o777)
+        os.chown("/var/tmp/voltage_and_current.csv", 1000, 1000)
+    
+    if(os.path.isfile('/var/tmp/voltage_and_current_log.csv')):
+        os.chmod("/var/tmp/voltage_and_current_log.csv", 0o777)
+        os.chown("/var/tmp/voltage_and_current_log.csv", 1000, 1000)
         
     else:
-        log_file_fd = open("/var/tmp/voltage_and_current_log","w+", encoding="utf_8", newline='\n')
-        log_file_fd.close()
-        os.chmod("/var/tmp/voltage_and_current_log", 0o777)
-        os.chown("/var/tmp/voltage_and_current_log", 1000, 1000)
+        with open('/var/tmp/voltage_and_current_log.csv', 'w+') as f:
+        os.chmod("/var/tmp/voltage_and_current_log.csv", 0o777)
+        os.chown("/var/tmp/voltage_and_current_log.csv", 1000, 1000)
     
     while(1):
         log_time = subprocess.check_output(['date', '+%Y年%m月%d日_%H時%M分%S秒'])
         timed_filename_str = log_time.decode(chardet.detect(log_time)["encoding"]).replace("\n", "")
-        # read current
-        Switched_mode_power_mA = Switched_mode_power.Read_mA()
-        Battery_power_mA = Battery_power.Read_mA()
-        SBC_power_mA = SBC_power.Read_mA()
-        Motor_power_mA = Motor_power.Read_mA()
-        # read volt
-        Switched_mode_power_mV = Switched_mode_power.Read_mV()
-        Battery_power_mV = Battery_power.Read_mV()
-        SBC_power_mV = SBC_power.Read_mV()
-        Motor_power_mV = Motor_power.Read_mV()
+        
+        read_data = []
+        # Read Switching_Power_Input
+        Switching_Power_Input_mA = Switching_Power_Input.Read_mA()
+        Switching_Power_Input_mV = Switching_Power_Input.Read_mV()
+        # Read Battery_Input_Power_Input
+        Battery_Input_mA = Battery_Input.Read_mA()
+        Battery_Input_mV = Battery_Input.Read_mV()
+        # Read SBC_Power_Supply
+        SBC_Power_Supply_mA = SBC_Power_Supply.Read_mA()
+        SBC_Power_Supply_mV = SBC_Power_Supply.Read_mV()
+        # Read Actuator_Power_Supply        
+        Actuator_Power_Supply_mA = Actuator_Power_Supply.Read_mA()
+        Actuator_Power_Supply_mV = Actuator_Power_Supply.Read_mV()
+        # Add data to the list
+        read_data.append(Switching_Power_Input_mA)
+        read_data.append(Switching_Power_Input_mV)
+        read_data.append(Battery_Input_mA)
+        read_data.append(Battery_Input_mV)
+        read_data.append(SBC_Power_Supply_mA)
+        read_data.append(SBC_Power_Supply_mV)
+        read_data.append(Actuator_Power_Supply_mA)
+        read_data.append(Actuator_Power_Supply_mV)
 
         # write tmp file
-        tmp_file_fd = open("/var/tmp/voltage_and_current","a", encoding="utf_8", newline='\n')
-        tmp_file_fd.write(
-            "Switched_mode_power_mA:{},\
-            Battery_power_mA:{},\
-            SBC_power_mA:{},\
-            Motor_power_mA:{},"
-            .format(
-                Switched_mode_power_mA, 
-                Battery_power_mA, 
-                SBC_power_mA, 
-                Motor_power_mA
-                )
-            )
-        tmp_file_fd.write(
-            "Switched_mode_power_mV:{},\
-            Battery_power_mV:{},\
-            SBC_power_mV:{},\
-            Motor_power_mV:{}, \n"
-            .format(
-                Switched_mode_power_mV,
-                Battery_power_mV,
-                SBC_power_mV,
-                Motor_power_mV
-                )
-            )
-
-        tmp_file_fd.close()
+        header = [
+            'Switching_Power_Input_mA',
+            'Switching_Power_Input_mV',
+            'Battery_Input_mA',
+            'Battery_Input_mV',
+            'SBC_Power_Supply_mA',
+            'SBC_Power_Supply_mV',
+            'Actuator_Power_Supply_mA',
+            'Actuator_Power_Supply_mV'
+        ]
         
-        # write log file
-        log_file_fd = open("/var/tmp/voltage_and_current_log","r+", encoding="utf_8", newline='\n')
-        lines = log_file_fd.readlines()
-        if len(lines) >= 1200:
-            lines = lines[1:]
-            log_file_fd.seek(0)
-            log_file_fd.truncate()
-            log_file_fd.writelines(lines)
+        with open('/var/tmp/voltage_and_current.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+            writer.writerow(read_data)
             
-        log_file_fd.write(
-            "{},\
-            Switched_mode_power_mA:{},\
-            Switched_mode_power_mV:{},\
-            Battery_power_mA:{},\
-            Battery_power_mV:{},\
-            SBC_power_mA:{},\
-            SBC_power_mV:{},\
-            Motor_power_mA:{}\
-			Motor_power_mV:{}\
-			\n"
-            .format(
-                timed_filename_str,
-                Switched_mode_power_mA, 
-                Switched_mode_power_mV,
-                Battery_power_mA, 
-                Battery_power_mV,
-                SBC_power_mA, 
-                SBC_power_mV,
-                Motor_power_mA,
-                Motor_power_mV
-                )
-            )
+        # write log file
+        with open('/var/tmp/voltage_and_current_log.csv', 'r+') as f:
+            lines = log_file_fd.readlines()
+            if len(lines) >= 1200:
+                lines = lines[2:]
+                log_file_fd.seek(0)
+                log_file_fd.truncate()
+                log_file_fd.writelines(lines)
+            
+            writer = csv.writer(f)
+            writer.writerow(header)
+            writer.writerow(read_data)
         
-        log_file_fd.close()
         time.sleep(0.5)
 
 if __name__ == '__main__':
