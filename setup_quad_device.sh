6de18dbcb825356d94ef1263005b44b4bc93673c
hiroki-0001 <touhu.273@gmail.com>
2023-06-15 18:19:12 +0900

    Add a script to set up the service

---
+++ setup_quad_device.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euxo pipefail
+
+HOME_DIR="${HOME}"
+
+if [ -e /etc/systemd/system/INA226VoltAndCurrent_quad.service ]; then
+	sudo systemctl disable INA226VoltAndCurrent_quad.service
+fi
+
+sudo ln -s $HOME_DIR/INA226_Library/service_files/INA226VoltAndCurrent_quad.service /etc/systemd/system/INA226VoltAndCurrent_quad.service
+sudo systemctl daemon-reload
+sudo systemctl enable INA226VoltAndCurrent_quad.service
+
+cat << EOS
+=========================================
+setup successful!
+
+please 
+
+"sudo systemctl start INA226VoltAndCurrent_quad.service" 
+or 
+reboot
+
+=========================================
+EOS
