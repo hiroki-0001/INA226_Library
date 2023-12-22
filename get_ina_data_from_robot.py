42ff0620ceaee7136841d5d25da66c4c5a2bb7ef
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2023-12-22 22:07:50 +0900

    インターネットのデバイスをlanかwifiで選択できるようにした

--- get_ina_data_from_robot.py
+++ get_ina_data_from_robot.py
@@ -4,14 +4,18 @@ import sys
 import subprocess
 
 argvs = sys.argv
-if len(argvs) < 2:
-    print("Usage: python get_ina_data_from_robot.py [target_robot(number)] optional:[output_file_name]")
+if len(argvs) < 3:
+    print("Usage: python get_ina_data_from_robot.py [target_robot(number)] [connect device(wifi or lan)] optional:[output_file_name]")
     sys.exit()
 
 target_robot = argvs[1]
+connect_device = argvs[2]
+if connect_device != "wifi" and connect_device != "lan":
+    print("connect_device Must be [lan or wifi]")
+    sys.exit()
 
-if len(argvs) == 3:
-    output_file_name = "./" + argvs[2]
+if len(argvs) == 4:
+    output_file_name = "./" + argvs[3]
 else:
     output_file_name = "."
 
@@ -23,8 +27,12 @@ except:
     print("Error: " + "command [sshpass] is not found. Please install.")
     sys.exit()
 
-subprocess.run(["sshpass -p cit" + target_robot + " ssh cit@192.168.4." + target_robot + " \"cd ~/INA226_Library/pyfiles; python3 read_log_data.py\""], shell=True)
-subprocess.run(["sshpass -p cit" + target_robot + " scp cit@192.168.4." + target_robot + ":~/INA226_Library/pyfiles/vol_and_cur_data.csv " + output_file_name],shell=True)
+if connect_device == "wifi":
+    subprocess.run(["sshpass -p cit" + target_robot + " ssh cit@192.168.4." + target_robot + " \"cd ~/INA226_Library/pyfiles; python3 read_log_data.py\""], shell=True)
+    subprocess.run(["sshpass -p cit" + target_robot + " scp cit@192.168.4." + target_robot + ":~/INA226_Library/pyfiles/vol_and_cur_data.csv " + output_file_name],shell=True)
+else:
+    subprocess.run(["sshpass -p cit" + target_robot + " ssh cit@192.168.100." + target_robot + " \"cd ~/INA226_Library/pyfiles; python3 read_log_data.py\""], shell=True)
+    subprocess.run(["sshpass -p cit" + target_robot + " scp cit@192.168.100." + target_robot + ":~/INA226_Library/pyfiles/vol_and_cur_data.csv " + output_file_name],shell=True)
 
 print("[[ Successfully read log data from " + target_robot + " ]]")
 print("[[ Saved as " + output_file_name + " ]]")
--- read_remote_robot_data.py
+++ read_remote_robot_data.py
@@ -4,12 +4,15 @@ import sys
 import subprocess
 
 argvs = sys.argv
-if len(argvs) != 2:
-    print("Usage: python get_ina_data_from_robot.py [target_robot(number)] ")
+if len(argvs) != 3:
+    print("Usage: python get_ina_data_from_robot.py [target_robot(number)] [connect devide(wifi or lan)] ")
     sys.exit()
 
 target_robot = argvs[1]
-
+connect_device = argvs[2]
+if connect_device != "wifi" and connect_device != "lan":
+    print("connect_device Must be [lan or wifi]")
+    sys.exit()
 
 print("Target_robot: " + target_robot)
 
@@ -19,4 +22,7 @@ except:
     print("Error: " + "command [sshpass] is not found. Please install.")
     sys.exit()
 
-subprocess.run(["sshpass -p cit" + target_robot + " ssh cit@192.168.4." + target_robot + " \"cd ~/INA226_Library/pyfiles; python3 read_latest_data.py\""], shell=True)
+if connect_device == "wifi":
+    subprocess.run(["sshpass -p cit" + target_robot + " ssh cit@192.168.4." + target_robot + " \"cd ~/INA226_Library/pyfiles; python3 read_latest_data.py\""], shell=True)
+else:
+    subprocess.run(["sshpass -p cit" + target_robot + " ssh cit@192.168.100." + target_robot + " \"cd ~/INA226_Library/pyfiles; python3 read_latest_data.py\""], shell=True)
