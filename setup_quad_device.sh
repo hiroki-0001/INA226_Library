e7f98d1b53b8e7916fd73853b7073a99b8b6a2fd
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2024-04-09 16:44:33 +0900

    update

--- setup_quad_device.sh
+++ setup_quad_device.sh
@@ -3,13 +3,13 @@ set -euxo pipefail
 
 HOME_DIR="${HOME}"
 
-if [ -e /etc/systemd/system/INA226VoltAndCurrent_quad.service ]; then
-	sudo systemctl disable INA226VoltAndCurrent_quad.service
+if [ -e /etc/systemd/system/INA226VoltAndCurrent_twin.service ]; then
+	sudo systemctl disable INA226VoltAndCurrent_twin.service
 fi
 
-sudo ln -s $HOME_DIR/INA226_Library/service_files/INA226VoltAndCurrent_quad.service /etc/systemd/system/INA226VoltAndCurrent_quad.service
+sudo ln -s $HOME_DIR/INA226_Library/service_files/INA226VoltAndCurrent_twin.service /etc/systemd/system/INA226VoltAndCurrent_twin.service
 sudo systemctl daemon-reload
-sudo systemctl enable INA226VoltAndCurrent_quad.service
+sudo systemctl enable INA226VoltAndCurrent_twin.service
 
 cat << EOS
 =========================================
